50154f5b3bd5014728bc8e90803f8009fc3d8d20
Ravi Prakash Pandey <59179046+ravi288@users.noreply.github.com>
2023-06-26 17:56:56 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchWeatherData = () => {
+    axios
+      .get(`http://localhost:8080/weather/${city}`, {})
+      .then((response) => {
+        setWeatherData(response.data);
+        setErrorMessage("");
+      })
+      .catch((error) => {
+        setWeatherData(null);
+        setErrorMessage("Error fetching weather data. Please try again.");
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather Prediction</h1>
+      <div>
+        <label htmlFor="city">City: </label>
+        <input
+          type="text"
+          id="city"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={fetchWeatherData}>Get Weather</button>
+      </div>
+      {errorMessage && <p>{errorMessage}</p>}
+      {weatherData && (
+        <div>
+          <h2>Weather for {weatherData.city}</h2>
+          <ul>
+            {weatherData.weatherForecastList.map((day) => (
+              <>
+                <table>
+                  <tr>
+                    <th>Date and Time</th>
+                    <th>Temperature</th>
+                    <th>High Temperature</th>
+                    <th>Low Temperature</th>
+                    <th>Weather Predicted</th>
+                  </tr>
+                  <tr>
+                    <td>{day.date}</td>
+                    <td>{day.temp} °f</td>
+                    <td>{day.highTemp} °f</td>
+                    <td>{day.lowTemp} °f</td>
+                    <td>{day.prediction}</td>
+                  </tr>
+                </table>
+              </>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
